dfb5f5deca06df38169d4475fb4c372f984fb14b
julieg18 <juliannagal18@gmail.com>
2020-10-05 14:18:42 -0500

    feat: update header with nav-items and links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-function Header() {
+function Header({ isUserLoggedIn, email, onSignOut }) {
+  const [areNavItemsOpen, setAreNavItemsOpen] = useState(false);
+  const location = useLocation();
+
+  function handleMenuBtnClick(e) {
+    setAreNavItemsOpen(!areNavItemsOpen);
+  }
+
   return (
-    <header className="header">
-      <div className="logo"></div>
+    <header
+      className={`header ${
+        areNavItemsOpen && isUserLoggedIn && 'header__nav-items-opened'
+      }`}
+    >
+      <div className="logo logo_place_header"></div>
+      {isUserLoggedIn ? (
+        <>
+          <ul
+            className={`header__nav-items ${
+              areNavItemsOpen && 'header__nav-items_opened'
+            }`}
+          >
+            <li className="header__nav-item">
+              <p className="header__user-email">{email}</p>
+            </li>
+            <li className="header__nav-item">
+              <button
+                onClick={onSignOut}
+                className="header__logout-btn hover-animation"
+              >
+                Log out
+              </button>
+            </li>
+          </ul>
+          <button
+            onClick={handleMenuBtnClick}
+            className={`hover-animation header__nav-items-btn header__nav-items-btn_image_${
+              areNavItemsOpen ? 'exit' : 'menu'
+            }`}
+          ></button>
+        </>
+      ) : (
+        <Link
+          className="header__link hover-animation"
+          to={location.pathname === '/signin' ? '/signup' : '/signin'}
+        >
+          {location.pathname === '/signin' ? 'Sign up' : 'Log in'}
+        </Link>
+      )}
     </header>
   );
 }
