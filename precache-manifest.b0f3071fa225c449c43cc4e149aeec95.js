e9efd3469dcc56edc2b16bb2a0d7b3bb42dd22f6
julieg18 <juliannagal18@gmail.com>
2020-10-07 07:15:52 -0500

    Updates

---
+++ precache-manifest.b0f3071fa225c449c43cc4e149aeec95.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d070bab00ab45d607be2dd0acbcb74",
+    "url": "/react-around-auth/index.html"
+  },
+  {
+    "revision": "f551ecbd7daa9b401f20",
+    "url": "/react-around-auth/static/css/main.f8cfad88.chunk.css"
+  },
+  {
+    "revision": "042ce653e60f3a72ca27",
+    "url": "/react-around-auth/static/js/2.84dc1810.chunk.js"
+  },
+  {
+    "revision": "9c0a6035e02563cb858933ba1cc2d4e4",
+    "url": "/react-around-auth/static/js/2.84dc1810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f551ecbd7daa9b401f20",
+    "url": "/react-around-auth/static/js/main.6e630721.chunk.js"
+  },
+  {
+    "revision": "5f3478028a5dc90c89ef",
+    "url": "/react-around-auth/static/js/runtime-main.c8c10083.js"
+  },
+  {
+    "revision": "14a176339fc00af3ae93be979f1593cc",
+    "url": "/react-around-auth/static/media/Inter-Black.14a17633.woff2"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "/react-around-auth/static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "41751fc730332b9facaea93b826197c1",
+    "url": "/react-around-auth/static/media/Inter-BlackItalic.41751fc7.woff"
+  },
+  {
+    "revision": "d92bb7894f88ca8ebf47e041ea8328e4",
+    "url": "/react-around-auth/static/media/Inter-BlackItalic.d92bb789.woff2"
+  },
+  {
+    "revision": "001893789f7f342b520f29ac8af7d6ca",
+    "url": "/react-around-auth/static/media/Inter-Bold.00189378.woff"
+  },
+  {
+    "revision": "e7ae98681edfa1df7f1e3ebba0d4fb88",
+    "url": "/react-around-auth/static/media/Inter-Bold.e7ae9868.woff2"
+  },
+  {
+    "revision": "5d543a76df6f812a369889f4eb5c3fa1",
+    "url": "/react-around-auth/static/media/Inter-BoldItalic.5d543a76.woff2"
+  },
+  {
+    "revision": "712a7862d8b61bf3c30d78d4a9b567bd",
+    "url": "/react-around-auth/static/media/Inter-BoldItalic.712a7862.woff"
+  },
+  {
+    "revision": "a0e89d60e007ea22dad528c5dec09cd4",
+    "url": "/react-around-auth/static/media/Inter-ExtraBold.a0e89d60.woff2"
+  },
+  {
+    "revision": "b82eef497995d28fffc2ef185211e282",
+    "url": "/react-around-auth/static/media/Inter-ExtraBold.b82eef49.woff"
+  },
+  {
+    "revision": "e3d4265af8ba0d48d6d20d1c18d46b6f",
+    "url": "/react-around-auth/static/media/Inter-ExtraBoldItalic.e3d4265a.woff"
+  },
+  {
+    "revision": "e92888792dd37175d8da0cfaabc7491e",
+    "url": "/react-around-auth/static/media/Inter-ExtraBoldItalic.e9288879.woff2"
+  },
+  {
+    "revision": "0713a6d4f4b4357758518720b363f322",
+    "url": "/react-around-auth/static/media/Inter-ExtraLight.0713a6d4.woff"
+  },
+  {
+    "revision": "8381bcfb1339ad96a5675d5dcfcbcd09",
+    "url": "/react-around-auth/static/media/Inter-ExtraLight.8381bcfb.woff2"
+  },
+  {
+    "revision": "3b8368534d20227d3187e9514e94f309",
+    "url": "/react-around-auth/static/media/Inter-ExtraLightItalic.3b836853.woff2"
+  },
+  {
+    "revision": "97eb194f3866a7edd06d012259039d27",
+    "url": "/react-around-auth/static/media/Inter-ExtraLightItalic.97eb194f.woff"
+  },
+  {
+    "revision": "2b94fbba68b9cd1f27d6a45d210cce99",
+    "url": "/react-around-auth/static/media/Inter-Italic.2b94fbba.woff2"
+  },
+  {
+    "revision": "5ddabe6d5f3898bd2f4bbf2d71e3548e",
+    "url": "/react-around-auth/static/media/Inter-Italic.5ddabe6d.woff"
+  },
+  {
+    "revision": "a1f9e860d918b33aa82a0c2c10d30d6f",
+    "url": "/react-around-auth/static/media/Inter-Light.a1f9e860.woff2"
+  },
+  {
+    "revision": "c58ea231468353018b9f8ca321f18282",
+    "url": "/react-around-auth/static/media/Inter-Light.c58ea231.woff"
+  },
+  {
+    "revision": "5cef43e5c2a3cc7efe510e791b70d79a",
+    "url": "/react-around-auth/static/media/Inter-LightItalic.5cef43e5.woff"
+  },
+  {
+    "revision": "778bddb259920029e780cbebc8d88f72",
+    "url": "/react-around-auth/static/media/Inter-LightItalic.778bddb2.woff2"
+  },
+  {
+    "revision": "95b8a98959d1af9ab432d7ffe295ef94",
+    "url": "/react-around-auth/static/media/Inter-Medium.95b8a989.woff"
+  },
+  {
+    "revision": "c709803c3cab6f1116039e881ecf531a",
+    "url": "/react-around-auth/static/media/Inter-Medium.c709803c.woff2"
+  },
+  {
+    "revision": "0011f9a53381655f19426b308c0b33ce",
+    "url": "/react-around-auth/static/media/Inter-MediumItalic.0011f9a5.woff"
+  },
+  {
+    "revision": "6b08fbd7a46708236caab921253c0763",
+    "url": "/react-around-auth/static/media/Inter-MediumItalic.6b08fbd7.woff2"
+  },
+  {
+    "revision": "1e081edc16d92d42aeccec760174fbf4",
+    "url": "/react-around-auth/static/media/Inter-Regular.1e081edc.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "/react-around-auth/static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "19b57197b819695d334b9961ee41910e",
+    "url": "/react-around-auth/static/media/Inter-SemiBold.19b57197.woff"
+  },
+  {
+    "revision": "4663322354d4300146ac57cd55daabf2",
+    "url": "/react-around-auth/static/media/Inter-SemiBold.46633223.woff2"
+  },
+  {
+    "revision": "30785b8aea4a0de8fe92363390bcbfd5",
+    "url": "/react-around-auth/static/media/Inter-SemiBoldItalic.30785b8a.woff2"
+  },
+  {
+    "revision": "32bb0e26464b0a6ff79016e6ad0af5bb",
+    "url": "/react-around-auth/static/media/Inter-SemiBoldItalic.32bb0e26.woff"
+  },
+  {
+    "revision": "035ae3d872fc8e111756711edc400bda",
+    "url": "/react-around-auth/static/media/Inter-Thin.035ae3d8.woff"
+  },
+  {
+    "revision": "9c96c7a2494ef60e8c2c75ad9baf1c5c",
+    "url": "/react-around-auth/static/media/Inter-Thin.9c96c7a2.woff2"
+  },
+  {
+    "revision": "4549a6c87a091a66fa84b4153116417a",
+    "url": "/react-around-auth/static/media/Inter-ThinItalic.4549a6c8.woff2"
+  },
+  {
+    "revision": "71242d414f94c2fb11533c9463825ba2",
+    "url": "/react-around-auth/static/media/Inter-ThinItalic.71242d41.woff"
+  },
+  {
+    "revision": "23dba457d138a98fff7b923daff6f387",
+    "url": "/react-around-auth/static/media/Inter-italic.var.23dba457.woff2"
+  },
+  {
+    "revision": "759e069c2a709969d41c53d3c4051ef1",
+    "url": "/react-around-auth/static/media/Inter-roman.var.759e069c.woff2"
+  },
+  {
+    "revision": "7e1eda3a103b1d4d14c9dea4ced21bfb",
+    "url": "/react-around-auth/static/media/Inter.var.7e1eda3a.woff2"
+  },
+  {
+    "revision": "256a3224b910c50fe46aba715b6b11a8",
+    "url": "/react-around-auth/static/media/avatar-image.256a3224.jpg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/react-around-auth/static/media/black-heart.9fdebb6b.svg"
+  },
+  {
+    "revision": "7fc4c149eaeefd2cf76f1e1b8ed62cb4",
+    "url": "/react-around-auth/static/media/check-symbol.7fc4c149.svg"
+  },
+  {
+    "revision": "c7df05485663b2b3ec3b99326f0f5ea2",
+    "url": "/react-around-auth/static/media/close.c7df0548.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/react-around-auth/static/media/edit-icon.58afbe73.svg"
+  },
+  {
+    "revision": "6c74d9b53442a247e20750f5de0be939",
+    "url": "/react-around-auth/static/media/heart.6c74d9b5.svg"
+  },
+  {
+    "revision": "3e6893cd2eab107d2d5385c921e95382",
+    "url": "/react-around-auth/static/media/logo.3e6893cd.svg"
+  },
+  {
+    "revision": "6626623bc87113a21a066edc45614d4e",
+    "url": "/react-around-auth/static/media/menu.6626623b.svg"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "/react-around-auth/static/media/pencil.34c46850.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/react-around-auth/static/media/plus.bdfb8ebb.svg"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "/react-around-auth/static/media/trash.9c3b03ae.svg"
+  },
+  {
+    "revision": "c32299fa3125c41146aa8359b2307523",
+    "url": "/react-around-auth/static/media/x-symbol.c32299fa.svg"
+  }
+]);
